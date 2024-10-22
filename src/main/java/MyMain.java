69f9371930d9c82269201e09eacf5d096c9ae00e
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-10-22 11:00:54 -0700

    wrote hasFullHouse challenge problem

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,7 +1,17 @@
 import java.util.ArrayList;
 
 public class MyMain {
-    
+    /**
+     * Given an array of "cards" (integers from 1 to 13), determine whether it contains a full house.
+     * A full house is a set of five cards where 3 cards have one value and 2 cards have a different value.
+     * @param cards The cards that might contain a full house. Guaranteed not to be <code>null</code> or empty,
+     *              but might not have 5 cards.
+     * @return <code>true</code> if there is at least one full house in cards, <code>false</code> otherwise.
+     */
+    public static boolean hasFullHouse (int[] cards) {
+        // REPLACE WITH YOUR CODE
+        return false;
+    }
 
     public static void main(String[] args) {
         // Write some code here to test your methods!
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -2,7 +2,22 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTests {
+    @Test
+    public void testHasFullHouse() {
+        assertTrue(MyMain.hasFullHouse(new int[]{1, 1, 1, 2, 2}));
+        assertTrue(MyMain.hasFullHouse(new int[]{1, 2, 1, 2, 2}));
+        assertTrue(MyMain.hasFullHouse(new int[]{12, 12, 13, 13, 12}));
+        assertTrue(MyMain.hasFullHouse(new int[]{1, 1, 1, 2, 2, 2}));
+        assertTrue(MyMain.hasFullHouse(new int[]{1, 2, 3, 4, 3, 2, 3}));
+        assertTrue(MyMain.hasFullHouse(new int[]{7, 5, 5, 7, 5}));
+
+        assertFalse(MyMain.hasFullHouse(new int[]{1, 2, 3, 4, 5}));
+        assertFalse(MyMain.hasFullHouse(new int[]{2, 2, 3, 3, 5}));
+        assertFalse(MyMain.hasFullHouse(new int[]{1, 1, 1, 1, 1}));
+        assertFalse(MyMain.hasFullHouse(new int[]{2, 2, 4, 4, 6, 6}));
+        assertFalse(MyMain.hasFullHouse(new int[]{2, 2, 4, 4}));
+    }
 }
